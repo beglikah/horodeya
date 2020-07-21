20ea28f25db6cedd652bb652b1cd04bc9f78c1a2
Hristiyan Dimitrov <hdimitrov1511@gmail.com>
2020-07-21 13:50:07 +0300

    Добавки към ипей форма2

--- projects/views.py
+++ projects/views.py
@@ -1661,7 +1661,7 @@ def pay_epay_support(request, pk):
     context['EXP_TIME'] = '01.08.2020'
     context['DESCR'] = 'Test'
     context['user_id'] = request.user.id
-    context['project_id'] = support.id
+    context['project_id'] = support.project.id
 
     context['data'] = ('MIN='+context['MIN'] + '\nINVOICE='+str(context['INVOICE']) + '\nAMOUNT=' +
                        str(context['AMOUNT']) + '\nEXP_TIME='+context['EXP_TIME'] + '\nDESCR='+context['DESCR'])
