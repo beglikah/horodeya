7406c0bdf96ae8cb22b9821ebaab9af70ad0a717
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2020-08-02 10:35:41 +0300

    По-добър backup скрипт тестван успешно с loadfixtures

--- backup.sh
+++ backup.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")"
 TIMESTAMP=`/bin/date +%Y-%m-%d_%H:%M:%S`
 /bin/mkdir -p backup
-bash manage.sh dumpdata --natural-foreign --format yaml -o backup/$TIMESTAMP.yaml -e auth.Permission -e sessions -e admin.logentry --exclude contenttypes
+bash manage.sh dumpdata --natural-primary --natural-foreign --format yaml -o backup/$TIMESTAMP.yaml -e sessions -e admin.logentry
