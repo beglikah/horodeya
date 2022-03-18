23ff234ed20543bd34015e0a98e84c1d6fdf542c
scallopy <scallopy@abv.bg>
2022-03-18 18:37:45 +0200

    ...

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,7 @@ from django.utils.encoding import force_text
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY='i#ksyj-un)vq^47g0_)%0uc$#mhgbe(y3@y8l_mpup94h#=!#b'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
